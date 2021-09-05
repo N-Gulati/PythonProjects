c4aaf4782895913e95e0762141761beed2a85453
NGULATI-PC\NGulati <nrgarg2004@gmail.com>
2021-09-05 12:04:36 -0400

    Removed inappropriately placed codes

--- ScoreBoardProcess.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pytesseract
-import pandas
-import matplotlib
-import scipy
-import numpy
-
--- Untitled-1.py
+++
@@ -1,5 +0,0 @@
-test = "test_string"
-print(test)
-print(str(type(test)))
-if str(type(test)) == "<class 'str'>":
-    print('true')
